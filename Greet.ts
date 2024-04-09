3178fc93f4dbc8770b5247f28540e8fbdfa90f5d
Mthunzi Tom <mthunzitom.10@gmail.com>
2024-04-09 11:56:50 +0200

    Enhanced modularity in Greet.ts with UserGreetCounter

--- Greet.ts
+++ Greet.ts
@@ -1,5 +1,6 @@
 import GreetIn from "./GreetIn";
 import { language } from "./Language";
+import UserGreetCounter from "./UserGreetCounterInt";
 
 export class GreetInXhosa implements GreetIn {
     greet(name: string): string {
@@ -22,16 +23,30 @@ export class GreetInTswana implements GreetIn {
 
 export default class Greeter {
     private greetLanguages: Map<language, GreetIn>
+    private userGreetCounter: UserGreetCounter
 
-    constructor(greetLanguages: Map<language, GreetIn>) {
+    constructor(greetLanguages: Map<language, GreetIn>, userGreetCounter: UserGreetCounter) {
         this.greetLanguages = greetLanguages;
+        this.userGreetCounter = userGreetCounter;
     }
 
     greet(name: string, chosenLanguage: language) {
         let greetIn = this.greetLanguages.get(chosenLanguage);
+
+        this.userGreetCounter.countGreet(name);
         
         if (greetIn) {
             return greetIn.greet(name);
         };
+
+        return "";
+    };
+
+    public get greetCounter(): number {
+        return this.userGreetCounter.greetCounter;
+    };
+
+    userGreetCount(firstName: string): number {
+        return this.userGreetCounter.userGreetCount(firstName);
     };
 }
--- test/User.test.ts
+++ test/User.test.ts
@@ -9,19 +9,27 @@ greetMap.set(language.eng, new GreetInEnglish);
 greetMap.set(language.tswana, new GreetInTswana);
 greetMap.set(language.xhosa, new GreetInXhosa);
 
-let greeter = new Greeter(greetMap);
-
 it("should greet in Xhosa", () => {
+    let userGreetCounterMap = new MapUserGreetCounter();
+    let greeter = new Greeter(greetMap, userGreetCounterMap);
+
     const xhosaGreeter = greeter.greet("FakeUserOne", language.xhosa);
     assert.equal("Molo FakeUserOne", xhosaGreeter);
 });
 
-it("should greet in Twana", () => { 
+it("should greet in Twana", () => {
+
+    let userGreetCounterMap = new MapUserGreetCounter();
+    let greeter = new Greeter(greetMap, userGreetCounterMap);
+
     const tswanaGreeter = greeter.greet("FakeUserTwo", language.tswana);
     assert.equal("Dumela FakeUserTwo", tswanaGreeter);
 });
 
-it("should greet in English", () => { 
+it("should greet in English", () => {
+    let userGreetCounterMap = new MapUserGreetCounter();
+    let greeter = new Greeter(greetMap, userGreetCounterMap);
+
     const englishGreeter = greeter.greet("FakeUserThree", language.eng);
     assert.equal("Hello FakeUserThree", englishGreeter);
 });
@@ -29,23 +37,39 @@ it("should greet in English", () => {
 // Testing UserGreetCounter implementation
 
 it("should increment greet counter", () => {
+
     let userGreetCounterMap = new MapUserGreetCounter();
+    let greeter = new Greeter(greetMap, userGreetCounterMap);
 
-    userGreetCounterMap.countGreet("User");
-    userGreetCounterMap.countGreet("UserOne");
-    userGreetCounterMap.countGreet("UserTwo");
+    greeter.greet("User", language.eng);
+    greeter.greet("UserOne", language.xhosa);
+    greeter.greet("UserTwo", language.eng);
 
-    assert.equal(3, userGreetCounterMap.greetCounter);
+    assert.equal(3, greeter.greetCounter);
+
+    greeter.greet("UserThree", language.xhosa);
+    greeter.greet("UserFour", language.eng);
+
+    assert.equal(5, greeter.greetCounter);
+    
 });
 
 it("should get user greets", () => {
+
     let userGreetCounterMap = new MapUserGreetCounter();
+    let greeter = new Greeter(greetMap, userGreetCounterMap);
 
-    userGreetCounterMap.countGreet("User");
-    userGreetCounterMap.countGreet("User");
-    userGreetCounterMap.countGreet("UserOne");
-    userGreetCounterMap.countGreet("UserTwo");
-    userGreetCounterMap.countGreet("UserTwo");
+    greeter.greet("User", language.tswana);
+    greeter.greet("User", language.xhosa);
+    greeter.greet("UserOne", language.xhosa);
+    greeter.greet("UserTwo", language.eng);
+    greeter.greet("UserTwo", language.eng);
 
-    assert.equal(2, userGreetCounterMap.userGreetCount("User"));
+    assert.equal(2, greeter.userGreetCount("User"));
+
+    greeter.greet("UserOne", language.xhosa);
+    greeter.greet("UserTwo", language.eng);
+
+    assert.equal(3, greeter.userGreetCount("UserTwo"));
+
 });
